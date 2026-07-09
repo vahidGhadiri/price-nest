43f08e06953a3bd5d3ec7d80e8745b654365cac6
vahid ghadiri <whydrf@gmail.com>
2026-07-09 22:43:04 +0330

    chore(global.d.ts): Add fundemental DTO's of result pattern

--- apps/client/global.d.ts
+++ apps/client/global.d.ts
@@ -1,14 +1,27 @@
 declare global {
-  type QueryValueType =
-    | (boolean | string | number)[]
-    | undefined
-    | boolean
-    | string
-    | number
-    | null;
+    type QueryValueType = (boolean | string | number)[] | undefined | boolean | string | number | null;
 
-  type QueryParams = Record<string, QueryValueType> | string;
-  type PathParams = Record<string, string | number>;
+    type QueryParams = Record<string, QueryValueType> | string;
+    type PathParams = Record<string, string | number>;
+
+    export type ErrorType = {
+        fieldErrors?: Record<string, string>;
+        severity: ErrorSeverity;
+        code: ErrorCode;
+        message: string;
+    };
+
+    type Result<T> =
+        | {
+              error: ErrorType;
+              success: false;
+              data: null;
+          }
+        | {
+              success: true;
+              error: null;
+              data: T;
+          };
 }
 
 export {};
