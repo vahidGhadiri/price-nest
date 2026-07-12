b6bd73fd52c2bd037665263ed91651ffd4cf06e2
vahid ghadiri <whydrf@gmail.com>
2026-07-12 10:37:03 +0330

    fix(next.config): Add output configuration

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    output: 'standalone',
 };
 
 const withNextIntl = createNextIntlPlugin('./src/configs/i18n/helpers/request.ts');
